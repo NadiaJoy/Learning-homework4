8c42fdca11a768a6e5204a8dda40170de065f5c7
NadiaJoy <iminneverwhere@gmail.com>
2023-02-10 15:50:29 +0200

    Homework 4. Part 1 + Part 2 ex.1-5

---
+++ src/Client.java
@@ -0,0 +1,17 @@
+public class Client {
+    String name;
+    double balance;
+    long personalCode;
+
+    public Client(String name, double balance, long personalCode) {
+        this.name = name;
+        this.balance = balance;
+        this.personalCode = personalCode;
+    }
+
+    public String greetings () {
+       String tere = "Tere";
+       return tere;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //
+        //Part 1
         Scanner input = new Scanner(System.in);
+
         System.out.println("Hi! I can calculate the volume of your figure. "
                 + "\nEnter its height: ");
         int h = input.nextInt();
@@ -15,10 +16,22 @@ public class Main {
 
         Figure figure = new Figure();
         int figureVolume = figure.calcVolume(h, w, l);
-
+        //ex.4
         System.out.println("Volume = " + figureVolume);
-
+        //ex.5
         figure.calcSurfaceArea();
+
+        //Part 2
+        //ex.3
+        Client perepelkin = new Client("Perepelkin Petr", 256.3, 325131234);
+        Client ivanova =  new Client("Ivanov Jan", 1458.95, 366010100);
+        Client tamm = new Client("Tamm Anna", 998.44, 491020112);
+        //ex.5
+        String tereClient = perepelkin.greetings() +", "+ perepelkin.name;
+        System.out.println(tereClient);
+
+
+
 
     }
 }
